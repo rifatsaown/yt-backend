0d31fdb262f888306bd6b66349d7610566c3e92d
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-01-04 05:19:40 +0600

    Index.html added

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8');
+    res.send(fs_1.default.readFileSync(path_1.default.resolve(__dirname, '../public/index.html'), 'utf-8'));
 });
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  // const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8');
+  // res.send(html);
+  res.send(fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8'));
 });
 
 app.listen(port, () => {
