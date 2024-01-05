8f8a58b8aa68e0e8c7fcdce9e40ff4c51d7eabcb
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-01-05 06:11:36 +0600

    database connected

---
+++ src/db/dbConnect.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async()=> {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`)
+        console.log(`MongoDB Connected: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
+import "dotenv/config";
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
+import connectDB from './db/dbConnect';
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  // const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8');
-  // res.send(html);
-  res.send(fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8'));
-});
+connectDB();
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+
+
+
+// app.get('/', (req, res) => {
+//   // const html = fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8');
+//   // res.send(html);
+//   res.send(fs.readFileSync(path.resolve(__dirname, '../public/index.html'), 'utf-8'));
+// });
