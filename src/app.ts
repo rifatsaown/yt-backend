4829f0f0cc60f8fb4af5ae96d398d3e97fcd2b6b
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-01-19 02:29:33 +0600

    comment done

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(cors(
 // express middleware for parsing json and urlencoded request body and static files
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static("public")); // serve static files from public folder
 
 // Cokie parser middleware
 app.use(cookieParser());
