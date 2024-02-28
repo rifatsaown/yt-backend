fff12aface4a34e54b4f39135571c5901b0675eb
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-02-28 16:02:34 +0600

    Reorder import statements in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from "cors";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from 'express';
 
 const app = express();
 
