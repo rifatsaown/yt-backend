c115835776947ba5b7f82a5ac6f9c50ed533cee2
Md Rifat Hossen <rifatsaown0@gmail.com>
2024-02-28 16:03:33 +0600

    Reorder import statements in asyncHandler.ts

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,5 +1,5 @@
 // Purpose: Express middleware to handle async errors. This middleware will be used in place of try catch block in async functions.
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 const asyncHandler = (requestHandler: (req: Request, res: Response, next: NextFunction) => Promise<void>) => {
     return (req: Request, res: Response, next: NextFunction) => {
